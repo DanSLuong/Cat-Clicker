8822e5bc057345b4f5ac3a9dd73045f8625dca8b
DanSLuong <hayate58@gmail.com>
2018-03-28 02:53:09 -0500

    Updated to meet the part 2 requirements from the Udacity FSND requirements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var countClick = function (e) {
-    if (e.target.id == "catPic") {
+    if (e.target.id == "catPic1") {
         output = output1;
+    }
+    else if (e.target.id == "catPic2") {
+        output = output2;
     }
     if (output.textContent) {
         output.textContent = parseInt(output.textContent) + count;
@@ -9,11 +12,17 @@ var countClick = function (e) {
         output.textContent = count;
     }
 }
-var e = document.getElementById('catPic');
-var output1 = document.getElementById('clickCount');
+var catPic1 = document.getElementById('catPic1');
+var output1 = document.getElementById('clickCount1');
+
+var catPic2 = document.getElementById('catPic2');
+var output2 = document.getElementById('clickCount2');
 
 var count = 1;
 
-if (e.addEventListener) {
-    e.addEventListener('click', countClick, false);
+if (catPic1.addEventListener) {
+    catPic1.addEventListener('click', countClick, false);
+}
+if (catPic2.addEventListener) {
+    catPic2.addEventListener('click', countClick, false);
 }
